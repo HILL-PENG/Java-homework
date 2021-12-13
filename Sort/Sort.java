87fcf7192fbdccd41d7b730e32f3d436d3d7de23
hill peng <iampengl@163.com>
2021-12-13 21:51:40 +0800

    update: bubble sort

--- Sort/Sort.java
+++ Sort/Sort.java
@@ -1,8 +1,12 @@
 package Sort;
 
-public class Bubble {
+public class Sort {
     public static void main(String[] args) {
         int[] arr = {8, 1, 3, 6, 9, 2, 4, 7, 5};
+        bubbleSort(arr);
+    }
+
+    public static void bubbleSort(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - 1 - i; j++) {
                 if (arr[j] > arr[j + 1]) {
